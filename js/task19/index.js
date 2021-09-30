ac0c252f743886c8f9f7070ff6193ff14eb15f7c
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-09-30 12:05:37 +0300

    form http

---
+++ js/task19/index.js
@@ -0,0 +1,31 @@
+const emailInput = document.getElementById('email');
+const nameInput = document.getElementById('name');
+const passwordInput = document.getElementById('password');
+const loginForm = document.forms[0];
+const submitBtn = document.querySelector('.submit-button');
+const errorText = document.querySelector('.error-text');
+const baseUrl = 'https://6141977c357db50017b3db7a.mockapi.io/api/v1/users2/'
+
+//1
+// const user = {
+//   email: emailInput.value,
+//   name: nameInput.value,
+//   password: passwordInput.value,
+// };
+
+//2
+const createUserHandler = (event)=>{
+  event.preventDefault();
+const user = Object.fromEntries(new FormData(loginForm));
+   fetch(baseUrl, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(user),
+  }).then(response=> response.json())
+  .then(body=>alert(body))
+
+
+    // eslint-disable-next-line no-return-assign
+    .catch(() => {errorText.textContent = 'Failed to create user'});
+};
+loginForm.addEventListener('submit', createUserHandler);
